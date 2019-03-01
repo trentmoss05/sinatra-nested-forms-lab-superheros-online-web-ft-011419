14d004531d4b538b3432ec8e06ef8d3b2fafa9fb
Trent Moss <trentmoss05@gmail.com>
2019-03-01 18:24:25 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -2,9 +2,15 @@ class SuperHero
 
   attr_reader :name, :power, :bio
 
+  @@all = []
+
   def initialize(args)
     @name = args[:name]
     @power = args[:power]
     @bio = args[:bio]
+  end
+
+  def self.all
+    @@all
   end
 end
